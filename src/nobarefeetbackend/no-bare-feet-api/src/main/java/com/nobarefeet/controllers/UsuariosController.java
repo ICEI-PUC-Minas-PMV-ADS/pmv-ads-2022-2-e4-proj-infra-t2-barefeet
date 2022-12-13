f5c689ba7b24973cab4d9dae0f13c45300cb99d0
Pedro Henrique Pinto de Lacerda <pedrohenrique73490@gmail.com>
2022-12-13 13:17:34 -0300

    Criando a lógica para usuarios e anuncio.

--- src/nobarefeetbackend/no-bare-feet-api/src/main/java/com/nobarefeet/controllers/UsuariosController.java
+++ src/nobarefeetbackend/no-bare-feet-api/src/main/java/com/nobarefeet/controllers/UsuariosController.java
@@ -3,19 +3,18 @@ package com.nobarefeet.controllers;
 import com.nobarefeet.models.Usuario;
 import com.nobarefeet.repositories.UsuarioRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @AllArgsConstructor
 public class UsuariosController {
     private final UsuarioRepository repository;
 
-    @RequestMapping(value = "/usuarios", method = RequestMethod.POST)
+    @PostMapping(value = "/usuarios")
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario saveUsuario(@RequestBody Usuario usuario) {
         repository.save(usuario);
@@ -25,5 +24,36 @@ public class UsuariosController {
     @GetMapping("/usuarios")
     public List<Usuario> getUsuarios() {
         return repository.findAll();
+    }
+
+    @DeleteMapping(value = "/usuarios/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarUsuario(@PathVariable("id") Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+    }
+
+    @PutMapping(value = "/usuarios/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Usuario editarUsuario(@PathVariable("id") Integer id, @RequestBody Usuario usuario) {
+        var optionalUsuario = repository.findById(id);
+        if(optionalUsuario.isPresent()) {
+            Usuario usuarioASerAlterado = optionalUsuario.get();
+            if (Objects.nonNull(usuario.getNome())) {
+                usuarioASerAlterado.setNome(usuario.getNome());
+            }
+            if (Objects.nonNull(usuario.getCep())) {
+                usuarioASerAlterado.setCep(usuario.getCep());
+            }
+            if (Objects.nonNull(usuario.getEmail())) {
+                usuarioASerAlterado.setEmail(usuario.getEmail());
+            }
+            if (Objects.nonNull(usuario.getDataNascimento())) {
+                usuarioASerAlterado.setDataNascimento(usuario.getDataNascimento());
+            }
+            return repository.save(usuarioASerAlterado);
+        }
+        return null;
     }
 }
--- src/nobarefeetbackend/no-bare-feet-api/src/main/java/com/nobarefeet/repositories/AnuncioRepository.java
+++ src/nobarefeetbackend/no-bare-feet-api/src/main/java/com/nobarefeet/repositories/AnuncioRepository.java
@@ -1,9 +1,9 @@
 package com.nobarefeet.repositories;
 
-import com.nobarefeet.models.Usuario;
+import com.nobarefeet.models.Anuncio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface AnuncioRepository extends JpaRepository<Anuncio, Integer> {
 }
